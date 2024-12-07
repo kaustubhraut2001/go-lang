41665c3e6e6756617988dd75b01d2ef9e857f280
kaustubhraut2001 <kaustubhr2001@gmail.com>
2024-12-07 21:45:40 +0530

    taking input in go

--- fir.go
+++ fir.go
@@ -2,6 +2,12 @@ package main
 
 import "fmt"
 
-func main(){
+func main() {
 	fmt.Println("Hello, Fir!")
+	// user input
+	var input int
+	fmt.Scanf("%d", &input)
+	fmt.Printf("You entered: %d\n", input)
+	// loop to print numbers from 1 to input
+
 }
