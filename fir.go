c8bac120c63ceb829c42f9e31ba42090bb133bda
kaustubhraut2001 <kaustubhr2001@gmail.com>
2024-12-07 22:07:27 +0530

    api calling in go

--- fir.go
+++ fir.go
@@ -1,13 +1,46 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
+
+type Todo struct {
+	UserID    int    `json:"userId"`
+	ID        int    `json:"id"`
+	Title     string `json:"title"`
+	Completed bool   `json:"completed"`
+}
 
 func main() {
 	fmt.Println("Hello, Fir!")
-	// user input
+
+	// User input
 	var input int
 	fmt.Scanf("%d", &input)
 	fmt.Printf("You entered: %d\n", input)
-	// loop to print numbers from 1 to input
 
+	resp, err := http.Get("https://jsonplaceholder.typicode.com/todos")
+	if err != nil {
+		log.Fatalf("Failed to make HTTP request: %v", err)
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalf("Failed to read response body: %v", err)
+	}
+
+	var todos []Todo
+	err = json.Unmarshal(body, &todos)
+	if err != nil {
+		log.Fatalf("Failed to unmarshal JSON: %v", err)
+	}
+
+	for _, todo := range todos {
+		fmt.Printf("%+v\n", todo)
+	}
 }
