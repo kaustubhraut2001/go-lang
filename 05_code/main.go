8da906535e56cbbed4c29f0ce32a5fd31fe98ba1
kaustubhraut2001 <kaustubhr2001@gmail.com>
2024-12-10 22:21:36 +0530

    go pointers

--- 05_code/main.go
+++ 05_code/main.go
@@ -12,4 +12,17 @@ func main() {
 	// make allocate memory but init non-zero storage
 
 	fmt.Println("Go Memeory management")
+
+	// pointers in go ang
+	fmt.Println("pointers in go")
+	var ptr *int
+	fmt.Println("VAule of pointers", ptr) // nil is default value
+
+	number := 23
+	var pt = &number
+	fmt.Println(*pt, pt) // pt will retunr address and *pt will return actual value
+
+	*pt = *pt + 2
+	fmt.Println(number)
+
 }
