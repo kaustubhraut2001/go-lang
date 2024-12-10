a16190ffd155f8ff6701e5503747fd929a27cc95
kaustubhraut2001 <kaustubhr2001@gmail.com>
2024-12-10 22:13:11 +0530

    go memory management key methods

---
+++ 05_code/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	// we make use o new or make
+
+	// garbage colection happesn automatically
+
+	// the differnace betwen the new and make is thatconst
+	// new allocates memory but no init  zeroed storage
+	// make allocate memory but init non-zero storage
+
+	fmt.Println("Go Memeory management")
+}
