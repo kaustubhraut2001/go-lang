7bcb31e354740d6dc13ef75e23ea7c8373058df2
kaustubhraut2001 <kaustubhr2001@gmail.com>
2024-12-10 21:29:05 +0530

    comma ok syntax

--- 02_code/main.go
+++ 02_code/main.go
@@ -10,6 +10,8 @@ func main() {
 	fmt.Println("go cod")
 	reader := bufio.NewReader(os.Stdin)
 	fmt.Print("Enter your name: ")
+	// comma ok syntax
 	name, _ := reader.ReadString('\n')
 	fmt.Printf("Hello, %s", name)
+	fmt.Printf("Type of this is %T \n", name)
 }
