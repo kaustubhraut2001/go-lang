730fe1c3ed9534e6d12a9a8c2dde4944105f189b
kaustubhraut2001 <kaustubhr2001@gmail.com>
2024-12-11 22:01:52 +0530

    hashaps and printing with llops

---
+++ 08_code/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Maps in go")
+
+	fruitMap := make(map[string]string)
+	fruitMap["Apple"] = "red"
+	fruitMap["Banana"] = "yellow"
+	fmt.Println(fruitMap["Apple"], fruitMap)
+
+	delete(fruitMap, "Banana") // to delete some things from map
+	fmt.Println(fruitMap)
+
+	// loops on hashmap
+
+	for key, value := range fruitMap {
+		fmt.Printf("Key : %s , Value : %s \n", key, value)
+	}
+}
